16de6b1d6e696d13a3c1f904d4909c1909cc6c72
XargonWan <XargonWan@gmail.com>
2024-12-10 22:16:41 +0900

    feat(libretro_binding): build checkpoint

--- libretro_binding/libretro_binding.h
+++ libretro_binding/libretro_binding.h
@@ -1,12 +1,41 @@
-// libretro_binding.h
-class LibretroCoreBinding : public GodotObject {
+#ifndef LIBRETRO_CORE_BINDING_H
+#define LIBRETRO_CORE_BINDING_H
+
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+#include <godot_cpp/variant/packed_byte_array.hpp>
+#include <godot_cpp/classes/ref.hpp>
+#include <godot_cpp/classes/object.hpp> // Correct base class
+
+// Typedefs for libretro core functions
+typedef void (*retro_init_t)();
+typedef void (*retro_deinit_t)();
+typedef bool (*retro_load_game_t)(const char *);
+typedef void (*retro_run_t)();
+typedef void (*retro_get_video_frame_t)(void **, unsigned *, unsigned *);
+
+class LibretroCoreBinding : public godot::Object {
+    GDCLASS(LibretroCoreBinding, godot::Object);
+
+private:
+    void *core_handle = nullptr;
+    retro_init_t retro_init = nullptr;
+    retro_deinit_t retro_deinit = nullptr;
+    retro_load_game_t retro_load_game = nullptr;
+    retro_run_t retro_run = nullptr;
+    retro_get_video_frame_t retro_get_video_frame = nullptr;
+
+    unsigned frame_width = 0;
+    unsigned frame_height = 0;
+    godot::PackedByteArray frame_buffer;
+
 public:
-    // Metodi da esporre a GDScript
-    bool initialize();
-    bool load_game(const PackedByteArray& rom_data);
+    bool initialize(const godot::String &core_path);
+    bool load_game(const godot::PackedByteArray &rom_data);
     void run();
-    PackedByteArray get_frame_buffer();
-    int get_frame_width();
-    int get_frame_height();
-    void handle_input(const Ref<InputEvent>& event);
+    godot::PackedByteArray get_frame_buffer();
+    int get_frame_width() const;
+    int get_frame_height() const;
 };
+
+#endif // LIBRETRO_CORE_BINDING_H
