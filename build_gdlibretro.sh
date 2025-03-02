71d350f77227047ebbb6b727eaf7a5e9414fe966
Humberto Dias <hldtux@gmail.com>
2025-03-02 13:04:18 -0300

    [ci] use build tmp directory

--- build_gdlibretro.sh
+++ build_gdlibretro.sh
@@ -6,8 +6,8 @@
 #git clone https://github.com/gabrielmedici/gdlibretro
 cd gdlibretro
 git submodule update --init --recursive && \
-cmake -DNO_GIT_REVISION=ON -DCMAKE_BUILD_TYPE=Debug -DLINUX=true -DCMAKE_CXX_FLAGS="-DLINUX" . && \
-cmake --build . && \
+cmake -DNO_GIT_REVISION=ON -DCMAKE_BUILD_TYPE=Debug -DLINUX=true -DCMAKE_CXX_FLAGS="-DLINUX" -Bbuild && \
+cmake --build build && \
 cd - && \
-mv -fv "gdlibretro/LibRetroHost/lib/Linux-x86_64/libLibRetroHost-d.so" "addons" && \
-rm -rf "gdlibretro/LibRetroHost"
+mv -fv "gdlibretro/build/LibRetroHost/lib/Linux-x86_64/libLibRetroHost-d.so" "addons" && \
+rm -rf "gdlibretro/build"
