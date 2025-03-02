69093df5e3b5e290ea43644c7ffce6889693086f
Humberto Dias <hldtux@gmail.com>
2025-03-02 13:36:35 -0300

    add cd workflow

--- build_gdlibretro.sh
+++ build_gdlibretro.sh
@@ -4,10 +4,13 @@
 
 # Disabled as now we are editing our own "fork"of libretro
 #git clone https://github.com/gabrielmedici/gdlibretro
+ARCH=$(uname -m)
+OS=$(uname -s)
 cd gdlibretro
 git submodule update --init --recursive && \
 cmake -DNO_GIT_REVISION=ON -DCMAKE_BUILD_TYPE=Debug -DLINUX=true -DCMAKE_CXX_FLAGS="-DLINUX" -Bbuild && \
 cmake --build build && \
 cd - && \
-mv -fv "gdlibretro/build/LibRetroHost/lib/Linux-x86_64/libLibRetroHost-d.so" "addons" && \
-rm -rf "gdlibretro/build"
+mv -fv "gdlibretro/build/LibRetroHost/lib/${OS}-${ARCH}/libLibRetroHost-d.so" "addons" && \
+rm -rf "gdlibretro/build" && \
+zip -9 gdlibretro-${OS}-${ARCH}.zip addons
