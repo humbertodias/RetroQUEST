ea60bf035230b489e59f12c31fd29c4f413c3a48
Humberto Dias <hldtux@gmail.com>
2025-03-03 00:23:45 -0300

    using find instead of mv

--- build_gdlibretro.sh
+++ build_gdlibretro.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
+set -e  # Exit on error
 
-# TODO: create a proer fork of the gdlibretro when ready and ask for a PR
+# TODO: create a proper fork of the gdlibretro when ready and ask for a PR
 
 # Disabled as now we are editing our own "fork"of libretro
-#git clone https://github.com/gabrielmedici/gdlibretro
-
-set -e  # Exit on error
+# git clone https://github.com/gabrielmedici/gdlibretro
 
 OS=$(uname -s)
 ARCH=$(uname -m)
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 BUILD_DIR="build"
-OUTPUT_ZIP="gdlibretro-${OS}-${ARCH}.zip"
-
 echo "Building for ${OS}-${ARCH}"
 
 cd gdlibretro
 git submodule update --init --recursive
-cmake -DNO_GIT_REVISION=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-Wformat-nonliteral -Werror -Wno-shadow" -B "${BUILD_DIR}"
+cmake -DNO_GIT_REVISION=ON -DCMAKE_BUILD_TYPE=Debug -B "${BUILD_DIR}"
 cmake --build "${BUILD_DIR}"
-
-# Move built files
-mv -fv "${BUILD_DIR}/LibRetroHost/lib/${OS}-*/libLibRetroHost-d.*" "${SCRIPT_DIR}/addons/"
-
-# Clean up and zip
+find . -name "libLibRetroHost-d.*" -exec mv -v {} "${SCRIPT_DIR}/addons/" \;
 rm -rf "${BUILD_DIR}"
-
 cd "${SCRIPT_DIR}"
-zip -r -9 "${OUTPUT_ZIP}" addons/*LibRetroHost*
-
-echo "Build complete: ${OUTPUT_ZIP}"
+zip -r -9 "gdlibretro-${OS}-${ARCH}.zip" addons/*LibRetroHost*
