2738f994f103dee2957e770fe06d57cd94af18b9
Humberto Dias <hldtux@gmail.com>
2025-03-03 01:27:08 -0300

    zip -r -9 "gdlibretro-${{ runner.os }}-${{ runner.arch }}.zip"

--- build_gdlibretro.sh
+++ build_gdlibretro.sh
@@ -6,17 +6,10 @@ set -e  # Exit on error
 # Disabled as now we are editing our own "fork"of libretro
 # git clone https://github.com/gabrielmedici/gdlibretro
 
-OS=$(uname -s)
-ARCH=$(uname -m)
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-BUILD_DIR="build"
-echo "Building for ${OS}-${ARCH}"
-
 cd gdlibretro
 git submodule update --init --recursive
-cmake -DNO_GIT_REVISION=ON -DCMAKE_BUILD_TYPE=Debug -B "${BUILD_DIR}"
-cmake --build "${BUILD_DIR}"
+cmake -DNO_GIT_REVISION=ON -DCMAKE_BUILD_TYPE=Debug -B build
+cmake --build build
 find . -name "libLibRetroHost-d.*" -exec mv -v {} "${SCRIPT_DIR}/addons/" \;
-rm -rf "${BUILD_DIR}"
-cd "${SCRIPT_DIR}"
-zip -r -9 "gdlibretro-${OS}-${ARCH}.zip" addons/*LibRetroHost*
+rm -rf build
